8443888f6add235cde8480bbd657d14d39228ce4
nycgui <nyc.gui@gmail.com>
2022-04-06 23:58:41 -0300

    implementando rotas de produto

--- src/Routes.ts
+++ src/Routes.ts
@@ -5,6 +5,10 @@ import { DeleteUserController } from "./controllers/users/DeleteUserController";
 import { UpdateUserController } from "./controllers/users/UpdateUserController";
 import { AuthenticateUserController } from "./controllers/users/AuthenticateUserController";
 
+import { CreateProductController } from "./controllers/products/CreateProductController";
+import { DeleteProductController } from "./controllers/products/DeleteProductController";
+import { ListProductController } from "./controllers/products/ListProductController";
+import { ListProductNameController } from "./controllers/products/ListProductNameController";
 
 import { ensureAdmin } from "./middlewares/EnsureAdmin";
 import { ensureAuthenticated } from "./middlewares/EnsureAuthenticated";
@@ -15,8 +19,17 @@ const deleteUserController = new DeleteUserController();
 const updateUserController = new UpdateUserController();
 const authenticateUserController = new AuthenticateUserController();
 
+const createProductController = new CreateProductController();
+const deleteProductController = new DeleteProductController();
+const listProductController = new ListProductController();
+const listProductNameController = new ListProductNameController();
 
 const router = Router();
+
+router.post("/products", ensureAuthenticated, createProductController.handle);
+router.get("/products/list-by-id/:id",  listProductController.handle);
+router.get("/products/list-by-name/:name",  listProductNameController.handle);
+router.delete("/products/:id", ensureAuthenticated, ensureAdmin, deleteProductController.handle);
 
 router.post("/users", createUserController.handle);
 router.post("/login", authenticateUserController.handle);
