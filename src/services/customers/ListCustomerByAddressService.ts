7bfb3d4fc4f87c0a2f12c4d5e75be32cd618cf8e
pedrobenetti <pedrobenetti@outlook.com.br>
2022-04-07 20:29:04 -0300

    Service para pesquisa por endereço

---
+++ src/services/customers/ListCustomerByAddressService.ts
@@ -0,0 +1,27 @@
+import { getCustomRepository } from "typeorm";
+import { CustomersRepositories } from "../../repositories/CustomersRepositories";
+import { classToPlain } from "class-transformer";
+
+interface ICustomerList{
+    address: string;
+}
+
+class ListCustomerByAddressService { 
+    async execute({address} : ICustomerList) {
+        const customerRepositories = getCustomRepository(CustomersRepositories);
+
+        if (!address){
+            throw new Error ("Inform the address!");
+        }
+
+        const customer = await customerRepositories.findOne({ address });
+
+        if (customer == null){
+            throw new Error ("Customer not found!");
+        }
+ 
+        return classToPlain(customer);
+    }
+}
+
+export { ListCustomerByAddressService };
