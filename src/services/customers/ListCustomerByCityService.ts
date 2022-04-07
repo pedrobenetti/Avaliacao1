917f93f30878baf49ef8edf03a67f0a012b16c9b
pedrobenetti <pedrobenetti@outlook.com.br>
2022-04-07 20:29:23 -0300

    Service para pesquisa por cidade

---
+++ src/services/customers/ListCustomerByCityService.ts
@@ -0,0 +1,27 @@
+import { getCustomRepository } from "typeorm";
+import { CustomersRepositories } from "../../repositories/CustomersRepositories";
+import { classToPlain } from "class-transformer";
+
+interface ICustomerList{
+    city: string;
+}
+
+class ListCustomerByCityService { 
+    async execute({city} : ICustomerList) {
+        const customerRepositories = getCustomRepository(CustomersRepositories);
+
+        if (!city){
+            throw new Error ("Inform the city!");
+        }
+
+        const customer = await customerRepositories.findOne({ city });
+
+        if (customer == null){
+            throw new Error ("Customer not found!");
+        }
+ 
+        return classToPlain(customer);
+    }
+}
+
+export { ListCustomerByCityService };
