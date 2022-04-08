6cb2a4bbe830fc604ce57823d9af7d390dcf5922
nycgui <nyc.gui@gmail.com>
2022-04-07 23:12:00 -0300

    implementando query para consulta de produtos

--- src/services/products/ListProductService.ts
+++ src/services/products/ListProductService.ts
@@ -2,21 +2,34 @@ import { getCustomRepository } from "typeorm";
 import { ProductsRepositories } from "../../repositories/ProductsRepositories";
 
 interface IProductList {
-    id: string
+    id: string,
+    name: string
 }
 
 class ListProductService {
-    async execute({ id }: IProductList) {
+    async execute({ id, name }: IProductList) {
 
         const productsRepositories = getCustomRepository(ProductsRepositories);
 
-        if (!id) {
-            throw new Error("Informe o id ;(");
+        if (!id && !name) {
+            throw new Error("To consult a product, enter your id or name :)");
         }
 
-        const product = await productsRepositories.findOne({ id });
-        
-        if (product == null) {
+        var product = null
+
+        if (id && name) {
+            product = await productsRepositories.find({ where: [{ id: id }, { name: name }] })
+        }
+
+        if (id && product == null) {
+            product = await productsRepositories.find({ where: { id: id } })
+        }
+
+        if (name && product == null) {
+            product = await productsRepositories.find({ where: { name: name } })
+        }
+
+        if (product != null && product[0] == null) {
             throw new Error("Product not found ;(");
         }
 
